47176ed50599e5d089692ca8ec729662d63d48aa
YEJIN-LILY <57944153+YEJIN-LILY@users.noreply.github.com>
2021-01-29 20:51:31 +0900

    Update 16401.cpp

--- ICPC_Shinchon/Sorting&Binary Search/16401.cpp
+++ ICPC_Shinchon/Sorting&Binary Search/16401.cpp
@@ -1,101 +1,44 @@
 #include <iostream>
-
+#include <vector>
 #include <algorithm>
-
 using namespace std;
 
- 
+#define ll long long
+#define MAX 100000
 
-const int MAX = 1000000;
+ll snack[1000000];
+ll N,M,high,low,mid,answer;
 
- 
-
-int M, N;
-
-int snack[MAX];
-
- 
-
-bool possible(int len)
-
-{
-
-        int cnt = 0;
-
-        for (int i = 0; i < N; i++)
-
-                 cnt += snack[i] / len;
-
- 
-
-        //조건이 성립할 경우에만 true
-
-        if (cnt >= M)
-
-                 return true;
-
-        return false;
-
-}
-
- 
-
-int main(void)
-
-{
-
-        ios_base::sync_with_stdio(0);
-
-        cin.tie(0);
-
-        cin >> M >> N;
-
- 
-
-        int low = 1, high = 0;
-
-        for (int i = 0; i < N; i++)
-
-        {
-
-                 cin >> snack[i];
-
-                 //제일 긴 과자의 길이가 high
-
-                 high = max(high, snack[i]);
-
-        }
-
- 
-
-        int result = 0;
-
-        //전형적인 이분 탐색
-
-        while (low <= high)
-
-        {
-
-                 int mid = (low + high) / 2;
-
-                 if (possible(mid))
-
-                 {
-
-                         result = mid;
-
-                         low = mid + 1;
-
-                 }
-
-                 else
-
-                         high = mid - 1;
-
-        }
-
-        cout << result << "\n";
-
-        return 0;
-
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	
+	cin>>M>>N;
+	
+	for(ll i=0;i<N;i++)
+		cin>>snack[i];
+	
+	high=1e9;
+	low=1;
+	
+	while(high>=low){
+		mid=(high+low)/2;
+		
+		ll sum=0;
+		
+		for(ll i=0;i<N;i++){
+			sum+=snack[i]/mid; //mid 길이만큼 나눠준다고 했을 때의 인원수
+			
+			if(sum>=M)
+				break;
+		}
+		
+		if(sum>=M){
+			answer=mid;
+			low=mid+1;
+		}else
+			high=mid-1;
+	}
+	
+	cout<<answer<<"\n";
 }
