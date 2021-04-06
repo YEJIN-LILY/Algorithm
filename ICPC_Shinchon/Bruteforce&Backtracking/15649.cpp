0e8804ac025f79b2c5691c85b5343e4394a42db8
YEJIN-LILY <57944153+YEJIN-LILY@users.noreply.github.com>
2021-04-06 16:37:38 +0900

    Update 15649.cpp

--- ICPC_Shinchon/Bruteforce&Backtracking/15649.cpp
+++ ICPC_Shinchon/Bruteforce&Backtracking/15649.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#define MAX 10
-
 using namespace std;
-int n,m;
 
-bool isused[MAX];
-int answer[MAX];
+int N,M;
+bool isused[10]; //사용 여부
+int answer[10]; //출력용
 
-void solution(int depth){
-	if(depth==m){
-		for(int i=0;i<depth;i++)
+void solution(int length){
+	if(length==M){ //길이 다 차면
+		for(int i=0;i<M;i++){
 			cout<<answer[i]<<' ';
-		cout<<'\n';
+		}
+		cout<<"\n";
+		return;
 	}
-	
-	for(int i=1;i<=n;i++){
-		if(isused[i])
+	for(int i=1;i<=N;i++){ //수열 숫자들 선택
+		if(isused[i])//이미 사용했으면 넘어감
 			continue;
-		isused[i]=true;
-		answer[depth]=i;
-		solution(depth+1);
-		isused[i]=false;
+		isused[i]=true; //선택
+		answer[length]=i;
+		solution(length+1); //다음 숫자 탐색
+		isused[i]=false; //선택 안 했을 경우
 	}
 }
 
-int main() {
+int main(){
 	ios_base::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
-
-	cin>>n>>m;
+	cin.tie(0),cout.tie(0);
 	
+	cin>>N>>M;
 	solution(0);
 }
