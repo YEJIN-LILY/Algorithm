cf42bdaf6f862b0d2a3a1dd560d53fa1f3104f11
YEJIN-LILY <57944153+YEJIN-LILY@users.noreply.github.com>
2021-04-11 18:36:59 +0900

    Create 11726.cpp

---
+++ ICPC_Shinchon/DP/11726.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0),cout.tie(0);
+	
+	cin>>n;
+	
+	dp[1]=1;
+	dp[2]=2;
+	
+	for(int i=3;i<=n;i++)
+		dp[i]=(dp[i-1]+dp[i-2])%10007;
+	
+	cout<<dp[n]%10007;
+}
